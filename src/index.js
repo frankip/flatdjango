b4d7c7fbd735f60d7789f5416f4c527d4ad49659
Francis <gwadafrank@gmail.com>
2022-09-07 15:45:13 +0300

    fetch data from server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 // Your code here
+
+const baseUrl = "http://localhost:3000";
+
+
+const movieList = document.getElementById('films')
+const movieImage = document.getElementById('poster')
+const movieShowing =  document.getElementById('showing')
+
+
+// console.log(movieImage, movieShowing);
+
+async function fetchData(){
+    const response = await fetch(`${baseUrl}/films`)
+    const data = await response.json()
+
+    data.forEach(element => {
+        console.log(element)
+    })
+
+}
+
+fetchData()
