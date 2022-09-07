3f430bc7c2cffb1ca004b5d618575e1515cd4917
Francis <gwadafrank@gmail.com>
2022-09-07 15:46:08 +0300

    fetch and update the movie list

--- src/index.js
+++ src/index.js
@@ -10,12 +10,23 @@ const movieShowing =  document.getElementById('showing')
 
 // console.log(movieImage, movieShowing);
 
+function renderMovies(movie){
+  let listElement = document.createElement("li");
+  listElement.className = "film item";
+  listElement.innerHTML = movie.title;
+
+  return listElement;
+}
+
 async function fetchData(){
     const response = await fetch(`${baseUrl}/films`)
     const data = await response.json()
 
     data.forEach(element => {
-        console.log(element)
+        let listElement = renderMovies(element)
+
+        movieList.appendChild(listElement)
+        // console.log(element)
     })
 
 }
