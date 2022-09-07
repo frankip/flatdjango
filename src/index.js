c242dc87a410e6c378aed2b702240216a8d30c0c
Francis <gwadafrank@gmail.com>
2022-09-07 15:51:18 +0300

    clean the codebase

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const movieImage = document.getElementById('poster')
 const movieShowing =  document.getElementById('showing')
 
 
-// console.log(movieImage, movieShowing);
-
+// render the movie titles 
 function renderMovies(movie){
   let listElement = document.createElement("li");
   listElement.className = "film item";
@@ -19,10 +18,11 @@ function renderMovies(movie){
           updateMovieDetails(movie);
       }, false)
 
-
   return listElement;
 }
 
+
+// updating the movie poster details
 function updateMovieDetails(movie){
   movieImage.src = movie.poster;
   movieImage.alt = movie.title;
@@ -36,15 +36,17 @@ function updateMovieDetails(movie){
     movie.capacity - movie.tickets_sold;
 }
 
+
+// fetching data function 
 async function fetchData(){
+    // fetch data
     const response = await fetch(`${baseUrl}/films`)
     const data = await response.json()
 
+    // loop through the returned data and pass to rendermovies
     data.forEach(element => {
         let listElement = renderMovies(element)
-
         movieList.appendChild(listElement)
-        // console.log(element)
     })
 
 }
