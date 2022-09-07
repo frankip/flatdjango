6a676521687724ded84eb6858f61e3f5d8052244
Francis <gwadafrank@gmail.com>
2022-09-07 15:47:04 +0300

    update the movie poster details

--- src/index.js
+++ src/index.js
@@ -15,7 +15,25 @@ function renderMovies(movie){
   listElement.className = "film item";
   listElement.innerHTML = movie.title;
 
+  listElement.addEventListener("click", (e) =>{
+          updateMovieDetails(movie);
+      }, false)
+
+
   return listElement;
+}
+
+function updateMovieDetails(movie){
+  movieImage.src = movie.poster;
+  movieImage.alt = movie.title;
+
+  // posterDetails
+  movieShowing.querySelector(".title").textContent = movie.title;
+  movieShowing.querySelector("#runtime").textContent = movie.runtime;
+  movieShowing.querySelector("#film-info").textContent = movie.description;
+  movieShowing.querySelector("#showtime").textContent = movie.showtime;
+  movieShowing.querySelector("#ticket-num").textContent =
+    movie.capacity - movie.tickets_sold;
 }
 
 async function fetchData(){
